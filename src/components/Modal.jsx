b6e0a2592908a11d6f30b50e7996e0a9008525c9
Cansu Öztaş <fcansuoztas@gmail.com>
2022-11-08 15:46:34 +0300

    edit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Modal = () => {
-    const navigate = useNavigate()
-    const {state} = useLocation()
-    console.log(state)
+  const navigate=useNavigate()
+
+
+  const locals=()=>{
+    localStorage.clear()
+  }
+   
+  const { state } = useLocation();
+  console.log(state);
+  console.log("nfkdnfk");
 
   return (
     <div className="modals">
       <div className="modals2">
+        
         <img src={state?.volumeInfo.imageLinks?.smallThumbnail} alt="empty" />
         <div className="title">
-         <button onClick={()=> navigate(-1)} >X</button>
+          <i  onClick={()=>navigate(-1 ,{locals} )  } className="fa-solid fa-x i"></i>
           <h2> {state?.volumeInfo?.title} </h2>
           <h3> {state?.volumeInfo?.authors?.[0]}</h3>
           <p>
-            {state?.volumeInfo.publisher} {state?.volumeInfo.publishedDate}
+            {" "}
+            {state?.volumeInfo.publisher} {state?.volumeInfo.publishedDate}{" "}
           </p>
-          <a href={state?.volumeInfo?.canonicalVolumeLink}>
+         <a href={state.volumeInfo.canonicalVolumeLink}>
             <button className="btn btn-primary">More</button>
           </a>
         </div>
@@ -27,6 +36,6 @@ const Modal = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Modal
+export default Modal;
